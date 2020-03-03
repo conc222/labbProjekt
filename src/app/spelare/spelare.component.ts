5c3545ae54c651dd3e5bc1edcd7187347a48f006
Cleas <cleas.olsen@hotmail.com>
2020-03-03 19:28:10 +0100

    projekt

--- src/app/spelare/spelare.component.ts
+++ src/app/spelare/spelare.component.ts
@@ -7,22 +7,19 @@ import { FotspelareListaService} from '../services/fotspelare-lista.service';
   templateUrl: './spelare.component.html',
   styleUrls: ['./spelare.component.scss']
 })
-constructor(private _name?: string, private _age?: number, private _personnr?: number, private _land?: string, private _position?: number) {}
 
 export class SpelareComponent implements OnInit {
-  spelare: Fotspelare[] = {
-    name = '',
-    age = null,
 
-  }
 
   constructor(private spelareService: FotspelareListaService) {
-
-
   }
 
   ngOnInit(): void {
-    this.spelare = this.spelareService.getSpelare();
+
+  }
+
+  addSpelare(f: Fotspelare) {
+
   }
 
 }
